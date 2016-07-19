47445867e11c2569a21df0959568f7c0c404baf1
Harry Jaggs <harryjaggs92@gmail.com>
2016-07-19 10:51:48 +0100

    step 7 start and restart

--- app.rb
+++ app.rb
@@ -3,3 +3,15 @@ require 'sinatra'
 get '/' do
   'hello!'
 end
+
+get '/secret' do
+  'secret message ...'
+end
+
+get '/greetings' do
+  'Yo my friend!'
+end
+
+get '/goodbye' do
+  'Bye, loser!'
+end
